bb44c257d1fec3d75f0de5ef6e60bd4f222c67e3
Davi Doro <ni.doro@gmail.com>
2025-12-28 22:35:21 -0300

    Customizes docs home page

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -11,32 +11,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Julia-first workflow',
+    Svg: require('@site/static/img/julia-dots.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Build web apps directly in Julia with automatic interactivity.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Zero-frontend setup',
+    Svg: require('@site/static/img/zero-setup.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Create full web interfaces without writing HTML, CSS, or JavaScript.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'High-performance data apps',
+    Svg: require('@site/static/img/high-perf.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Handle large data and complex computations with Julia’s performance.
       </>
     ),
   },
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -14,13 +14,13 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          <img src="/docs/build/img/lit-logo.png" /> {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/category/api-reference">
             Get Started
           </Link>
         </div>
