bce033c82b411587ea3a823ec8999600aaf2f1a6
Davi Doro <ni.doro@gmail.com>
2026-01-11 18:35:38 -0300

    Automates the creation of Artifacts.toml and version update on Project.toml

--- scripts/pack-artifacts.sh
+++ scripts/pack-artifacts.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Error: Version string required. Example: v1.0.0"
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+VERSION_STRING=$1
+
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $THIS_DIR/.. # Repo's root
 
@@ -12,16 +20,8 @@ docker run --rm -v "$PWD":/work lit-build:20.04 bash -c "./net-layer/build-linux
 ARTIFACTS_DIR=build/linux-x86_64/artifacts-linux-x86_64
 
 tar -czf "$ARTIFACTS_DIR.tar.gz" -C "$ARTIFACTS_DIR" .
-
-echo
-echo "-------------- Linux x86-64 Artifacts ----------------"
-echo "Git Tree hash"
-julia -e "using Pkg; using Pkg.GitTools; println(bytes2hex(Pkg.GitTools.tree_hash(\"$ARTIFACTS_DIR\")))"
-echo
-echo "Checksum of $ARTIFACTS_DIR.tar.gz"
-sha256sum "$ARTIFACTS_DIR.tar.gz" | cut -d' ' -f1
-echo "------------------------------------------------------"
-echo
+LINUX_X86_64_TREE_SHA1=$(julia -e "using Pkg; using Pkg.GitTools; println(bytes2hex(Pkg.GitTools.tree_hash(\"$ARTIFACTS_DIR\")))")
+LINUX_X86_64_SHA256=$(sha256sum "$ARTIFACTS_DIR.tar.gz" | cut -d' ' -f1)
 
 # Windows 64-bit
 #----------------------------
@@ -30,15 +30,37 @@ net-layer/build-win64.sh
 ARTIFACTS_DIR=build/win64/artifacts-win64
 
 tar -czf "$ARTIFACTS_DIR.tar.gz" -C "$ARTIFACTS_DIR" .
+WIN64_TREE_SHA1=$(julia -e "using Pkg; using Pkg.GitTools; println(bytes2hex(Pkg.GitTools.tree_hash(\"$ARTIFACTS_DIR\")))")
+WIN64_SHA256=$(sha256sum "$ARTIFACTS_DIR.tar.gz" | cut -d' ' -f1)
 
-echo
-echo "------------ Windows 64-bit Artifacts ----------------"
-echo "Git Tree hash"
-julia -e "using Pkg; using Pkg.GitTools; println(bytes2hex(Pkg.GitTools.tree_hash(\"$ARTIFACTS_DIR\")))"
-echo
-echo "Checksum of $ARTIFACTS_DIR.tar.gz"
-sha256sum "$ARTIFACTS_DIR.tar.gz" | cut -d' ' -f1
-echo "------------------------------------------------------"
-echo
+# Create Artifacts.toml
+#---------------------------
+echo "Creating Artifacts.toml..."
+echo "
+[[artifacts]]
+git-tree-sha1 = \"$LINUX_X86_64_TREE_SHA1\"
+os = \"linux\"
+arch = \"x86_64\"
+
+    [[artifacts.download]]
+    url = \"https://github.com/nidoro/Lit.jl/releases/download/$VERSION_STRING/artifacts-linux-x86_64.tar.gz\"
+    sha256 = \"$LINUX_X86_64_SHA256\"
+
+[[artifacts]]
+git-tree-sha1 = \"$WIN64_TREE_SHA1\"
+os = \"windows\"
+arch = \"x86_64\"
+
+    [[artifacts.download]]
+    url = \"https://github.com/nidoro/Lit.jl/releases/download/$VERSION_STRING/artifacts-win64.tar.gz\"
+    sha256 = \"$WIN64_SHA256\"
+" > Artifacts.toml
+
+# Update Project.toml version
+#------------------------------
+echo "Updating Project.toml..."
+julia -e "using TOML; d = TOML.parsefile(\"Project.toml\"); d[\"version\"] = \"$VERSION_STRING\"; open(\"Project.toml\", \"w\") do io; TOML.print(io, d) end"
+
+echo "Ok!"
 
 popd
