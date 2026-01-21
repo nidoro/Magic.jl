252a9c6c39314b49c1a4a49129677f57075d9f0c
Davi Doro <ni.doro@gmail.com>
2026-01-20 22:54:52 -0300

    icu build script for windows

---
+++ net-layer/third/build-icu-release-78.1-win64.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -xe
+
+export TARGET=x86_64-w64-mingw32
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DEST_DIR=$THIS_DIR/../../build/win64/icu-release-78.1
+NATIVE_BUILD=$THIS_DIR/../../build/linux-x86_64/icu-release-78.1
+
+pushd $THIS_DIR/icu-release-78.1
+
+#make clean
+mkdir -p $DEST_DIR
+
+./icu4c/source/configure \
+    --with-cross-build="$NATIVE_BUILD" \
+    --host=$TARGET \
+    --build=x86_64-linux-gnu \
+    --prefix=$DEST_DIR \
+    --enable-static \
+    --disable-shared \
+    --disable-tools \
+    --disable-extras \
+    LDFLAGS="-lwinpthread" \
+    CFLAGS="-fPIC" \
+    CXXFLAGS="-fPIC"
+
+make -j8
+make install
+
+popd
