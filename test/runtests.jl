aaeeba3a0e7effd9953922d0f88823e1124bd985
Davi Doro <ni.doro@gmail.com>
2026-08-20 20:56:33 -0300

    Adds test 01-counter

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using Test
+using Revise
 using Magic
 
 const TESTS_TO_RUN = isempty(ARGS) ? nothing : ARGS
@@ -8,16 +9,20 @@ function should_run(name::String)::Bool
 end
 
 should_run("start_app") && @testset "function start_app(...) input validation" begin
-    @test_throws Magic.InvalidFile              start_app("__NON_EXISTING_FILE__", dot_magic_dir="../examples")
-    @test_throws Magic.InvalidHostname          start_app("../examples/app.jl", host_name="__INVALID_HOSTNAME__", dot_magic_dir="../examples")
-    @test_throws Magic.InvalidPort              start_app("../examples/app.jl", port=-1, dot_magic_dir="../examples")
-    @test_throws Magic.InvalidPort              start_app("../examples/app.jl", port=65535+1, dot_magic_dir="../examples")
-    @test_throws Magic.InvalidUploadMaxSize     start_app("../examples/app.jl", upload_max_size=-1, dot_magic_dir="../examples")
-    @test_throws Magic.InvalidUploadMaxFiles    start_app("../examples/app.jl", upload_max_files=-1, dot_magic_dir="../examples")
-    @test_throws Magic.InvalidDirectory         start_app("../examples/app.jl", dot_magic_dir="__NON_EXISTING_DIR__")
-    @test_throws Magic.InvalidDirectory         start_app("../examples/app.jl", docs_path="__NON_EXISTING_DIR__", dot_magic_dir="../examples")
+    @test_throws Magic.InvalidFile              start_app("__NON_EXISTING_FILE__", dev_mode=true, init_and_quit=true)
+    @test_throws Magic.InvalidHostname          start_app(host_name="__INVALID_HOSTNAME__", dev_mode=true, init_and_quit=true)
+    @test_throws Magic.InvalidPort              start_app(port=-1, dev_mode=true, init_and_quit=true)
+    @test_throws Magic.InvalidPort              start_app(port=65535+1, dev_mode=true, init_and_quit=true)
+    @test_throws Magic.InvalidUploadMaxSize     start_app(upload_max_size=-1, dev_mode=true, init_and_quit=true)
+    @test_throws Magic.InvalidUploadMaxFiles    start_app(upload_max_files=-1, dev_mode=true, init_and_quit=true)
+    @test_throws Magic.InvalidDirectory         start_app(dot_magic_dir="__NON_EXISTING_DIR__", dev_mode=true, init_and_quit=true)
+    @test_throws Magic.InvalidDirectory         start_app(docs_path="__NON_EXISTING_DIR__", dev_mode=true, init_and_quit=true)
 end
 
 should_run("examples_dry_run") && @testset "Examples dry run" begin
     @test start_app("../examples/app.jl", dot_magic_dir="../examples", dev_mode=true, init_and_quit=true) === nothing
+end
+
+should_run("01-counter") && @testset "Example: 01-counter" begin
+    @test start_app(dev_mode=true) === nothing
 end
