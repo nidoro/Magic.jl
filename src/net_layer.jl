58ea8a2d129b9882a6923cc83bff1485b15524aa
Davi Doro <ni.doro@gmail.com>
2026-09-13 23:54:12 -0300

    Fixes breaking changes on Julia 1.13

--- src/net_layer.jl
+++ src/net_layer.jl
@@ -1,4 +1,9 @@
 
+function get_net_layer_func(func_sym::Symbol)::Ptr
+    lib = Libdl.dlopen(g.MAGIC_SO)
+    return Libdl.dlsym(lib, func_sym)
+end
+
 function init_net_layer(
     host_name::String,
     port::Int,
@@ -12,7 +17,7 @@ function init_net_layer(
 )
 
     ccall(
-        (:MG_InitNetLayer, g.MAGIC_SO),
+        get_net_layer_func(:MG_InitNetLayer),
         Cvoid,
         (Cstring, Cint, Cint, Cstring, Cint, Cint, Cstring, Cint, Cstring, Cint, Cint, Cint, Cint),
         host_name, Cint(sizeof(host_name)), port, docs_path, Cint(sizeof(docs_path)), Cint(ipc_port), dot_magic_dir, Cint(sizeof(dot_magic_dir)), package_root_dir, Cint(sizeof(package_root_dir)), Cint(upload_max_size), Cint(verbose), Cint(dev_mode)
@@ -22,23 +27,23 @@ end
 function create_app_event(event_type::AppEventType, client_id::Cint, payload::Union{String, Nothing})::AppEvent
     payload_ptr = payload !== nothing ? payload : Ptr{Cchar}(0)
     payload_size = payload !== nothing ? Cint(sizeof(payload)) : Cint(0)
-    return ccall((:MG_CreateAppEvent, g.MAGIC_SO), AppEvent, (AppEventType, Cint, Ptr{Cchar}, Cint), event_type, client_id, payload_ptr, payload_size)
+    return ccall(get_net_layer_func(:MG_CreateAppEvent), AppEvent, (AppEventType, Cint, Ptr{Cchar}, Cint), event_type, client_id, payload_ptr, payload_size)
 end
 
 function destroy_net_event(ev::NetEvent)::Nothing
-    ccall((:MG_DestroyNetEvent, g.MAGIC_SO), Cvoid, (NetEvent,), ev)
+    ccall(get_net_layer_func(:MG_DestroyNetEvent), Cvoid, (NetEvent,), ev)
 end
 
 function server_is_running()::Bool
-    return ccall((:MG_ServerIsRunning, g.MAGIC_SO), Cint, ())
+    return ccall(get_net_layer_func(:MG_ServerIsRunning), Cint, ())
 end
 
 function do_service_work()::Int
-    return ccall((:MG_DoServiceWork, g.MAGIC_SO), Cint, ())
+    return ccall(get_net_layer_func(:MG_DoServiceWork), Cint, ())
 end
 
 function stop_server()
-    return ccall((:MG_StopServer, g.MAGIC_SO), Cvoid, ())
+    return ccall(get_net_layer_func(:MG_StopServer), Cvoid, ())
 end
 
 """
@@ -60,53 +65,53 @@ function get_server_port()::Int
 - [`get_query_params`](/docs/build/docs/api-reference/application-logic/get_query_params-func)
 """
 function get_server_port()::Int
-    return ccall((:MG_GetServerPort, g.MAGIC_SO), Cint, ())
+    return ccall(get_net_layer_func(:MG_GetServerPort), Cint, ())
 end
 
 function is_tls_enabled()::Bool
-    return ccall((:MG_IsTLSEnabled, g.MAGIC_SO), Cint, ())
+    return ccall(get_net_layer_func(:MG_IsTLSEnabled), Cint, ())
 end
 
 function is_https_enabled()::Bool
     if g.net_layer_ready
-        return ccall((:MG_IsHTTPSEnabled, g.MAGIC_SO), Cint, ())
+        return ccall(get_net_layer_func(:MG_IsHTTPSEnabled), Cint, ())
     else
         return false
     end
 end
 
 function lock_client(client_id::Cint)::Nothing
-    ccall((:MG_LockClient, g.MAGIC_SO), Cvoid, (Cint,), client_id)
+    ccall(get_net_layer_func(:MG_LockClient), Cvoid, (Cint,), client_id)
     return nothing
 end
 
 function unlock_client(client_id::Cint)::Nothing
-    ccall((:MG_UnlockClient, g.MAGIC_SO), Cvoid, (Cint,), client_id)
+    ccall(get_net_layer_func(:MG_UnlockClient), Cvoid, (Cint,), client_id)
     return nothing
 end
 
 function pop_net_event()::NetEvent
-    return ccall((:MG_PopNetEvent, g.MAGIC_SO), NetEvent, ())
+    return ccall(get_net_layer_func(:MG_PopNetEvent), NetEvent, ())
 end
 
 function push_app_event(app_event::AppEvent)::Nothing
-    ccall((:MG_PushAppEvent, g.MAGIC_SO), Cvoid, (AppEvent,), app_event)
+    ccall(get_net_layer_func(:MG_PushAppEvent), Cvoid, (AppEvent,), app_event)
     return nothing
 end
 
 function push_uri_mapping(uri::String, resource_path::String)::Nothing
-    ccall((:MG_PushURIMapping, g.MAGIC_SO), Cvoid, (Cstring, Cint, Cstring, Cint), uri, Cint(sizeof(uri)), resource_path, Cint(sizeof(resource_path)))
+    ccall(get_net_layer_func(:MG_PushURIMapping), Cvoid, (Cstring, Cint, Cstring, Cint), uri, Cint(sizeof(uri)), resource_path, Cint(sizeof(resource_path)))
     return nothing
 end
 
 function clear_uri_mapping()::Nothing
-    ccall((:MG_ClearURIMapping, g.MAGIC_SO), Cvoid, ())
+    ccall(get_net_layer_func(:MG_ClearURIMapping), Cvoid, ())
     return nothing
 end
 
 function get_mime_type(file_path::AbstractString)::String
     result = ccall(
-        (:MG_GetMimeType, g.MAGIC_SO),
+        get_net_layer_func(:MG_GetMimeType),
         Cstring,
         (Cstring,),
         file_path
